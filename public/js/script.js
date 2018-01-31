5f2a19ce94d612bdce677aebc84941497e56ee1a
Federico Ulfo <federicoulfo@gmail.com>
2018-01-31 12:38:07 -0500

    Add jQuery f-yeah

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,30 @@
-function hideAuthenticatedUser() {
-  document.getElementById("user-not-authenticated").classList.add("show");
-  document.getElementById("user-not-authenticated").classList.remove("hide");
-  document.getElementById("user-authenticated").classList.add("hide");
-  document.getElementById("user-authenticated").classList.remove("show");
-  document.getElementById("user-authenticated-signout").classList.add("hide");
-  document.getElementById("user-authenticated-signout").classList.remove("show");
-}
-function showAuthenticatedUser(user) {
-
-  var name = user.name ? user.name : "user";
-
-  document.getElementById("user-not-authenticated").classList.add("hide");
-  document.getElementById("user-not-authenticated").classList.remove("show");
-  document.getElementById("user-authenticated").classList.add("show");
-  document.getElementById("user-authenticated").classList.remove("hide");
-  document.getElementById("user-authenticated-signout").classList.add("show");
-  document.getElementById("user-authenticated-signout").classList.remove("hide");
-
-  document.getElementById("user-authenticated-name").innerHTML = "" + name;
+function hideAuthenticatedUser () {
+  $('#user-not-authenticated').addClass('show')
+  $('#user-not-authenticated').removeClass('hide')
+  $('#user-authenticated').addClass('hide')
+  $('#user-authenticated').removeClass('show')
+  $('#user-authenticated-signout').addClass('hide')
+  $('#user-authenticated-signout').removeClass('show')
 }
 
-function hideFirebaseLoginWindow() {
-  document.getElementById("firebaseui-auth-container").classList.add("hide");
+function showAuthenticatedUser (user) {
+
+  var name = (user.displayName ? user.displayName : 'user').toString()
+
+  $('#user-not-authenticated').addClass('hide')
+  $('#user-not-authenticated').removeClass('show')
+  $('#user-authenticated').addClass('show')
+  $('#user-authenticated').removeClass('hide')
+  $('#user-authenticated-signout').addClass('show')
+  $('#user-authenticated-signout').removeClass('hide')
+
+  $('#user-authenticated-name').innerHTML = name
 }
 
-function showFirebaseLoginWindow() {
-  document.getElementById("firebaseui-auth-container").classList.add("show");
+function hideFirebaseLoginWindow () {
+  $('#firebaseui-auth-container').addClass('hide')
+}
+
+function showFirebaseLoginWindow () {
+  $('#firebaseui-auth-container').addClass('show')
 }
